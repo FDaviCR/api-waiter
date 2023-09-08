59b12b4c9aeebdd698a4ed276303e8a8c67bad94
Davi <davicamiloribeiro@gmail.com>
2023-09-08 20:35:50 -0300

    Dependencias MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017')
+	.then(() => console.log('Conectado ao MongoDB'))
+	.catch(() => console.log('Erro ao conectar no MongoDB'));
 
 app.listen(7777, ()=>{
 	console.log('🖳 - Servidor está rodando na porta: 7777');
