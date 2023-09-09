9096c73710fcc5e3c963f229360ba21589966aa6
Davi <davicamiloribeiro@gmail.com>
2023-09-09 12:46:48 -0300

    Ajuste no endereço do MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017')
+	.then(() => {
+		console.log('Conectado ao MongoDB');
+		const app = express();
 
-mongoose.connect('mongodb://localhost:27017')
-	.then(() => console.log('Conectado ao MongoDB'))
-	.catch(() => console.log('Erro ao conectar no MongoDB'));
+		app.listen(7777, ()=>{
+			console.log('🖳 - Servidor está rodando na porta: 7777');
+		});
+	})
+	.catch((err) => {
+		console.log('Erro ao conectar no MongoDB');
+		console.log(err);
+	});
 
-app.listen(7777, ()=>{
-	console.log('🖳 - Servidor está rodando na porta: 7777');
-});
+
